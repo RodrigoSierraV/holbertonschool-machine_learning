5077f2268b260c6dd9c762963f93a83521581107
RodrigoSierraV <735@holbertonschool.com>
2020-04-15 15:32:38 +0000

    [Fix] Verify shape of matrix

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -11,7 +11,8 @@ def add_matrices2D(mat1, mat2):
     """
     verify_len1 = len(mat1) == len(mat2)
     if verify_len1:
-        verify_len2 = all([len(mat1[i]) == len(mat2[i]) for i in range(len(mat1))])
+        verify_len2 = all([len(mat1[i]) == len(mat2[i])
+                           for i in range(len(mat1))])
         if verify_len2:
             return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
                     for i in range(len(mat1))]
