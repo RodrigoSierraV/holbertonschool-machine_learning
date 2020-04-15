fd542bbbcce30cae8bce5b270815d8263843b53e
RodrigoSierraV <735@holbertonschool.com>
2020-04-15 15:37:30 +0000

    [Add] method that concatenates two arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Contains a method that concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+        Function that concatenates two arrays
+        Return: A new array
+    """
+    return arr1 + arr2
