4d93f0053201efee73b598187731ca57b6a83b8d
RodrigoSierraV <735@holbertonschool.com>
2020-04-15 14:42:49 +0000

    [Add] Function that adds two arrays element-wise

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -4,14 +4,11 @@ Contains a method that adds two arrays
 """
 
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
-
-
 def add_arrays(arr1, arr2):
     """
         Function that adds two arrays
         Return: A new array
     """
-    if matrix_shape(arr1) != matrix_shape(arr2):
+    if len(arr1) != len(arr2):
         return
     return [arr1[i] + arr2[i] for i in range(len(arr1))]
