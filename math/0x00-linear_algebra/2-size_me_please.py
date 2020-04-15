6c03deb02c8f5856bb8469fa93caf1a70bb487b5
RodrigoSierraV <735@holbertonschool.com>
2020-04-15 12:37:29 +0000

    [Fix] docstrings

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """
+        Recursive function that finds the size of a matrix
+        Return: as a list of integers
+    """
     if isinstance(matrix[0], list):
         return [len(matrix)] + matrix_shape(matrix[0])
     return [len(matrix)]
