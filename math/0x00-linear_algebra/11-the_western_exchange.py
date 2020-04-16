2ba5fbf65df51b751e3cf1e5118abe3477c7fe60
RodrigoSierraV <735@holbertonschool.com>
2020-04-16 16:15:19 +0000

    [Add] function that transposes  a matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains a method that obtains the transpose of a numpy.ndarray
+"""
+
+
+def np_transpose(matrix):
+    """
+        Function that obtains the transpose of a numpy.ndarray
+    """
+    return matrix.transpose()
