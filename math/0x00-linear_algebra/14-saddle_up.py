7058e205c236989c8b38f39578b2580cdaa76f37
RodrigoSierraV <735@holbertonschool.com>
2020-04-16 19:19:30 +0000

    [Add] function that performs matrix multiplication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Contains a method that performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Function that performs matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
