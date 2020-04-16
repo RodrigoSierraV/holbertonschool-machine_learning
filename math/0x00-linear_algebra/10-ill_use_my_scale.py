cb98b46e80b63102f7c58c776b8531016434f590
RodrigoSierraV <735@holbertonschool.com>
2020-04-16 16:07:31 +0000

    [Add] Function that calculates the shape of a numpy.ndarray

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains a method that performs matrix multiplication
+"""
+
+
+def np_shape(matrix):
+    """
+        Function that calculates the shape of a numpy.ndarray
+    """
+    return matrix.shape
