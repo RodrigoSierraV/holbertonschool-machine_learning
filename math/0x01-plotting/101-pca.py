7e03c72823b3755f9ae602bd5eaa7397b4d48802
RodrigoSierraV <735@holbertonschool.com>
2020-04-20 22:08:06 +0000

    [Add] 3D plot of PCA from iris data set

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -12,4 +12,10 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-print(pca_data, labels)
+ax = Axes3D(plt.figure(), )
+ax.scatter(pca_data[:, 0].T, pca_data[:, 1].T, pca_data[:, 2].T, c=labels, cmap=plt.cm.plasma)
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+ax.set_title('PCA of Iris Dataset')
+plt.show()
