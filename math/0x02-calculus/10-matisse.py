42955c6035cf1c4d325033cdd92e71f1da9bcf40
RodrigoSierraV <735@holbertonschool.com>
2020-04-24 17:44:26 +0000

    [Add] Module that calculates the derivative of a polynomial

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """calculates the derivative of a polynomial"""
+    verify = all([isinstance(i, int) for i in poly])
+    if verify is False:
+        return
+    return [poly[coef] * coef for coef in range(1, len(poly))]
