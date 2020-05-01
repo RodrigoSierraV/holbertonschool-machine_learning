d9a4cf9a62b1b794a691e7e006903cfdc89537ed
RodrigoSierraV <735@holbertonschool.com>
2020-05-01 08:06:13 +0000

    [Add] define Poisson

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+This module represents Poisson distribution
+"""
+
+
+class Poisson:
+    """
+    class that models the Poisson distribution
+    """
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """Class constructor
+        """
+        if data is None:
+            self.lambtha = float(lambtha)
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            self.lambtha = float(sum(data) / len(data))
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
