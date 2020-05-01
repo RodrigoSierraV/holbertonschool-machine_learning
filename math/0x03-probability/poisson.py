ba34677e68c89f83486010990963c895996037b3
RodrigoSierraV <735@holbertonschool.com>
2020-05-01 08:34:00 +0000

    [Add] PMF for Poisson

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -23,3 +23,22 @@ class Poisson:
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+
+    def factorial(self, num):
+        """Computes factorial of a number"""
+        return num * self.factorial(num - 1) if num > 1 else 1
+
+    def pmf(self, k):
+        """Calculates Probability Mass Function PMF”"""
+        e = 2.7182818285
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        exp = (e**(self.lambtha * (-1)))
+        average = self.lambtha**k
+        factorial = self.factorial(k)
+        pmf = (exp * average) / factorial
+
+        return pmf
