ce61242c35ebea7c30d91290d58da1b0b5dd8210
RodrigoSierraV <735@holbertonschool.com>
2020-05-24 21:22:10 -0500

    [Add] Mini-batch gradient descent

---
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+""" Module to train a Neural network with mini-batch gradient descent"""
+import numpy as np
+import tensorflow as tf
+
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+
+def create_mini_batches(X, Y, batch_size):
+    """Creates mini-batches from entire data"""
+    mini_batches = []
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
+    data = np.hstack((X_shuffled, Y_shuffled))
+    n_minibatches = data.shape[0] // batch_size
+    i = 0
+
+    for i in range(n_minibatches + 1):
+        mini_batch = data[i * batch_size:(i + 1) * batch_size, :]
+        X_mini = mini_batch[:, : -Y.shape[1]]
+        Y_mini = mini_batch[:, -Y.shape[1]:]
+        mini_batches.append((X_mini, Y_mini))
+    if data.shape[0] % batch_size != 0:
+        mini_batch = data[i * batch_size:data.shape[0]]
+        X_mini = mini_batch[:, :-Y.shape[1]]
+        Y_mini = mini_batch[:, -Y.shape[1]:]
+        mini_batches.append((X_mini, Y_mini))
+    return mini_batches
+
+
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
+                     batch_size=32, epochs=5,
+                     load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """Computes training with mini-batch gradient descent"""
+    with tf.Session() as session:
+        saver = tf.train.import_meta_graph(load_path + '.meta')
+        saver.restore(session, load_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        train_op = tf.get_collection('train_op')[0]
+        feed_train = {x: X_train, y: Y_train}
+        feed_valid = {x: X_valid, y: Y_valid}
+        m = X_train.shape[0]
+        for epoch in range(epochs):
+            train_cost = session.run(loss, feed_dict=feed_train)
+            train_accuracy = session.run(accuracy, feed_dict=feed_train)
+            valid_cost = session.run(loss, feed_dict=feed_valid)
+            valid_accuracy = session.run(accuracy, feed_dict=feed_valid)
+            print("After {} epochs:".format(epoch))
+            print("\tTraining Cost: {}".format(train_cost))
+            print("\tTraining Accuracy: {}".format(train_accuracy))
+            print("\tValidation Cost: {}".format(valid_cost))
+            print("\tValidation Accuracy: {}".format(valid_accuracy))
+            mini_batches = create_mini_batches(X_train, Y_train, batch_size)
+
+            for i in range(len(mini_batches)):
+                X_mini, Y_mini = mini_batches[i]
+                feed_batch = {x: X_mini, y: Y_mini}
+                session.run(train_op, feed_dict=feed_batch)
+                if i % 100 == 0 and i != 0:
+                    step_accuracy = session.run(accuracy, feed_dict=feed_batch)
+                    step_cost = session.run(loss, feed_dict=feed_batch)
+                    print("\tStep {}:".format(i))
+                    print("\t\tCost: {}".format(step_cost))
+                    print("\t\tAccuracy: {}".format(step_accuracy))
+        print("After {} epochs:".format(epoch + 1))
+        print("\tTraining Cost: {}".format(train_cost))
+        print("\tTraining Accuracy: {}".format(train_accuracy))
+        print("\tValidation Cost: {}".format(valid_cost))
+        print("\tValidation Accuracy: {}".format(valid_accuracy))
+        save_path = saver.save(session, save_path)
+    return save_path
