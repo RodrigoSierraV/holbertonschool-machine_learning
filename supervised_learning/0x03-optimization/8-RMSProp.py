7d803497e433f5609db8ba8d76cb58dcd907e4d9
RodrigoSierraV <735@holbertonschool.com>
2020-05-24 23:59:00 -0500

    [Add] RMSprop upgraded

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""RMSProp Upgraded"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """Tensorflow using the RMSProp optimization algorithm"""
+
+    return tf.train.RMSPropOptimizer(
+        learning_rate=alpha,
+        decay=beta2,
+        epsilon=epsilon
+        ).minimize(loss)
