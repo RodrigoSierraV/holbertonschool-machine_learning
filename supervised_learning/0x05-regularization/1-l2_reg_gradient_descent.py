cb70c640a4da7415e6eff515d139858c4132d497
RodrigoSierraV <735@holbertonschool.com>
2020-05-31 19:10:05 -0500

    [Fix] back prop regularized

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -14,15 +14,14 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         b = 'b{}'.format(layer)
         if layer == L:
             dz = cache[A] - Y
-            dw = (np.matmul(cache[A_prev], dz.T) / m).T
+            W = w[w_lay]
         else:
-            w_back = 'W{}'.format(layer + 1)
-            d1 = np.matmul(w[w_back].T, dz_prev)
+            d1 = np.matmul(W.T, dz)
             derive_tanh = 1 - cache[A] ** 2
             dz = d1 * derive_tanh
-            dw = np.matmul(dz, cache[A_prev].T) / m
-        dz_prev = dz
-        dw_reg = dw + (lambtha * w[w_lay] / m)
+            W = w[w_lay]
+        dw = np.matmul(cache[A_prev], dz.T) / m
+        dw_reg = dw.T + (lambtha * w[w_lay] / m)
         db = np.sum(dz, axis=1, keepdims=True)
 
         weights[w_lay] = w[w_lay] - alpha * dw_reg
