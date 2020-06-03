9e35c0c37088889b20e6fbf4366c9acd3e9069d1
RodrigoSierraV <735@holbertonschool.com>
2020-06-02 23:44:40 -0500

    [Fix] loss

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -7,6 +7,6 @@ def optimize_model(network, alpha, beta1, beta2):
     """ Optimize a keras model"""
     network.compile(
         optimizer=K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2),
-        loss=K.losses.CategoricalCrossentropy(),
+        loss='categorical_crossentropy',
         metrics=['accuracy']
     )
