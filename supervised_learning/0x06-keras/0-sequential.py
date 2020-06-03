ff4bbca5e2efe71af685f70697ff929d3981a68b
RodrigoSierraV <735@holbertonschool.com>
2020-06-02 23:13:17 -0500

    [Fix] import

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 """ Module to create a neural network with keras """
-import tensorflow as tf
-from tensorflow import keras
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ Create a neural network """
-    model = keras.Sequential()
+    model = K.Sequential()
     model.add(
-        keras.layers.Dense(
+        K.layers.Dense(
             layers[0],
             input_shape=(nx,),
             activation=activations[0],
-            kernel_regularizer=keras.regularizers.l2(lambtha)
+            kernel_regularizer=K.regularizers.l2(lambtha)
         )
     )
     for layer, activation in zip(layers[1:], activations[1:]):
-        model.add(keras.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dropout(1 - keep_prob))
         model.add(
-            keras.layers.Dense(
+            K.layers.Dense(
                 layer,
                 activation=activation,
-                kernel_regularizer=keras.regularizers.l2(lambtha)
+                kernel_regularizer=K.regularizers.l2(lambtha)
             )
         )
     return model
