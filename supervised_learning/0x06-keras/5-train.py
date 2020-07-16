2925eec7dcc7a90e7c82ec5f39d6bdad9121b831
RodrigoSierraV <735@holbertonschool.com>
2020-07-16 00:28:21 -0500

    [Add] update train model

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Updates the function train model"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    validation_data is the data to validate the model with, if not None
+    """
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data)
