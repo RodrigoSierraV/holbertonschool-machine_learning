57795cc528a09696188e9f491e3b0d8f53e6d1fc
RodrigoSierraV <735@holbertonschool.com>
2020-07-17 01:01:23 -0500

    [Fix] shapes

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
-""" Module to compute back propagation in a convolutional layer with pooling"""
+""" Module to compute back propagation in a convolutional layer with padding"""
 import numpy as np
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """ Back prop for NN layer"""
-    m, h_new, w_new, c_new = dZ.shape
-    kh, kw, c_prev, _ = W.shape
-    _, h_prev, w_prev, _ = A_prev.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    _, h_new, w_new, _ = dZ.shape
+    kh, kw, _, c_new = W.shape
     sh, sw = stride
-    if padding == 'same':
-        ph = ((h_new - 1) * sh + kh - h_new) // 2
-        pw = ((w_new - 1) * sw + kw - w_new) // 2
+
+    if padding == "same":
+        ph = ((sh * h_prev) - sh + kh - h_prev) // 2
+        pw = ((sw * w_prev) - sw + kw - w_prev) // 2
     else:
         ph, pw = 0, 0
-    pad_res = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+
+    pad_img = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                      mode='constant', constant_values=0)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+    dA = np.zeros(pad_img.shape)
     dW = np.zeros(W.shape)
-    db = np.sum(dZ, axis=(1, 2, 3), keepdims=True)
-    dA = np.zeros(A_prev.shape)
+
     for img in range(m):
         for row in range(h_new):
             for col in range(w_new):
                 for ch in range(c_new):
                     a = row * sh
-                    b = row * sh + kh
+                    b = a + kh
                     c = col * sw
-                    d = col * sw + kw
-                    dA[img, a:b, c:d, :] += W[:, :, :, ch]\
+                    d = c + kw
+                    dA[img, a:b, c:d, :] += dZ[img, row, col, ch]\
+                        * W[:, :, :, ch]
+                    dW[:, :, :, ch] += pad_img[img, a:b, c:d, :]\
                         * dZ[img, row, col, ch]
-                    dW[:, :, :, ch] += pad_res[img, a:b, c:d, :]\
-                        * dZ[img, row, col, ch]
+    if padding == 'same':
+        dA = dA[:, ph:-ph, pw: -pw, :]
     return dA, dW, db
