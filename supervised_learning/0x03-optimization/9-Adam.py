f72c22a2b2cdef73aff48bc8786f6d1897bbb68e
RodrigoSierraV <735@holbertonschool.com>
2020-07-16 21:57:22 -0500

    [Add] Adam

---
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module to implement Adam"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """
+    alpha is the learning rate
+    beta1 is the weight used for the first moment
+    beta2 is the weight used for the second moment
+    epsilon is a small number to avoid division by zero
+    var is a numpy.ndarray containing the variable to be updated
+    grad is a numpy.ndarray containing the gradient of var
+    v is the previous first moment of var
+    s is the previous second moment of var
+    t is the time step used for bias correction
+    Returns: the updated variable, the new first moment, and the new second
+        moment, respectively
+    """
+    mt = beta1 * v + (1 - beta1) * grad
+    vt = beta2 * s + (1 - beta2) * grad ** 2
+
+    m_hat = mt / (1 - beta1 ** t)
+    v_hat = vt / (1 - beta2 ** t)
+    var = var - alpha * (m_hat / (v_hat ** (1 / 2) + epsilon))
+    return var, m_hat, v_hat
