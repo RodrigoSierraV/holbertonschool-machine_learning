3b4ea7b18c1b0aa7a1c1c5c027f424b3c4353287
RodrigoSierraV <735@holbertonschool.com>
2020-07-24 21:14:03 -0500

    [Add] minor of a matrix

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -28,6 +28,6 @@ def determinant(matrix):
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     deter = 0
     for c in range(len(matrix)):
-        deter += ((-1.0) ** c) * matrix[0][c] *\
+        deter += ((-1) ** c) * matrix[0][c] *\
                  determinant(getMatrixMinor(matrix, 0, c))
-    return int(deter)
+    return deter
